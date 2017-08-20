bd42653847a2707d6936127a8f05291e7c0ef6df
onesirian <ldlockhart@gmail.com>
2017-08-20 16:48:17 -0500

    add js/script.js

--- js/script.js
+++ js/script.js
@@ -34,12 +34,19 @@ function loadData() {
            };
     }); 
     //Wikipedia Info
-    var $wikihead = $(#wikipedia-header);
-    var $wikilink = $(#wikipedia-links);
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
     $.ajax({
-        url:'https://en.wikipedia.org/w/api.php', 
-        data:'cityStr'})
-    $wikilink.text.append();
+        url: wikiUrl, 
+        dataType: "jsonp",
+        success: function(response){
+            var articleList = response[1];
+            for (var i=0; i<articleList.length; i++){
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki' + articleStr;
+                $wikiElem.append('<li><a href="' + url + '">'+articleStr +'</a></li>');
+            };
+            }   
+        });
   return false;
 };
 $('#form-container').submit(loadData);
