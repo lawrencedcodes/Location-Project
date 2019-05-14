2a64ad077e24d5a6b346e8a36347dbbcc7416a67
Lawrence Lockhart <ldlockhart@gmail.com>
2019-05-14 13:09:44 -0500

    Remove broken wiki, update NYT api

--- script.js
+++ script.js
@@ -33,24 +33,6 @@ function loadData() {
             '</li>');   
            };
     }); 
-    //Wikipedia Info
-    var wikiRequestTimeout = setTimeout(function(){
-        $wikiElem.text("failed to get wikipedia resources");
-    }, 8000);
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
-    $.ajax({
-        url: wikiUrl, 
-        dataType: "jsonp",
-        success: function(response){
-            var articleList = response[1];
-            for (var i=0; i<articleList.length; i++){
-                articleStr = articleList[i];
-                var url = 'http://en.wikipedia.org/wiki' + articleStr;
-                $wikiElem.append('<li><a href="' + url + '">'+articleStr +'</a></li>');
-            };
-            clearTimeout(wikiRequestTimeout);
-            }   
-        });
   return false;
 };
 $('#form-container').submit(loadData);
